f12ef7b491e6328e5fb689a1267265ddfb244fe4
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2023-01-28 22:35:12 +0000

    Hide API keys

--- firebase.js
+++ firebase.js
@@ -1,16 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-// TODO: add SDKs for Firebase prodjcts that you want to use
-
-const firebaseConfig = {
-    apiKey: 'AIzaSyDhRIMa9rkdPQ4rF6E9ME4kRbF9W7xItk4',
-    authDomain: 'fittrack-7bcf8.firebaseapp.com',
-    projectId: 'fittrack-7bcf8',
-    storageBucket: 'fittrack-7bcf8.appspot.com',
-    messagingSenderId: '287839198178',
-    appId: '1:287839198178:web:2cafb250359db56efb37bb'
-};
+import { firebaseConfig } from "./firebase-config";
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
