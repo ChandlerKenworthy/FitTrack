a2b69463fc6095bc596e42bf7f7cbeca2457e97e
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2023-03-17 11:41:18 +0000

    Update empty state to better structure

--- state/EmptyState.js
+++ state/EmptyState.js
@@ -6,7 +6,14 @@ export const EmptyExercise = {
 
 export const EmptyWorkout = {
     date: new Date(), // default's to today
-    exercises: {
-        // should have key: value pair as exercise_id: [[reps, reps, reps], [weight, weight, weight]]
-    },
+    exercises: [], // array of id's
+    sets: [], // how many sets each exercise was performed for
+    reps: [], // arr of arr (n reps in each set)
+    weights: [], // weight of each set (len(weights) = sum(sets)
+};
+
+export const EmptyWorkoutTemplate = {
+    exercises: [], // Array of exercises by exercise_id
+    sets: [], // how many sets each exercise should be peformed for
+    reps: [], // how many reps to perform in each set (arr of arr)
 };
