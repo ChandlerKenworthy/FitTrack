c2476327d5a133a575352618c8f6272e760fbdeb
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2023-03-29 15:19:33 +0100

    Make header color change with dark mode

--- components/navigation/CustomNavigationContainer.js
+++ components/navigation/CustomNavigationContainer.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { SettingsContext } from '../../store/settings-context'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { userAuthContext } from '../../store/UserAuthContext';
+import { colors } from '../../constants/Globalstyles';
 
 const CustomNavigationContainer = ({authStack, authenticatedStack}) => {
     const settingsCtx = useContext(SettingsContext);
@@ -11,7 +12,8 @@ const CustomNavigationContainer = ({authStack, authenticatedStack}) => {
         ...DefaultTheme,
         colors: {
             ...DefaultTheme.colors,
-            background: settingsCtx.darkMode ? '#1b1b1d' : '#f1f1f1'
+            background: settingsCtx.darkMode ? '#1b1b1d' : '#f1f1f1',
+            card: settingsCtx.darkMode ? '#242526' : colors.white,
         }
     };
 
