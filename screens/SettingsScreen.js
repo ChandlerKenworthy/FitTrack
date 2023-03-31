513f8b006d418267d17e9e8c542fa6021df5bf9e
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2023-03-31 19:09:50 +0100

    Dark mode styles for settings screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -7,30 +7,35 @@ import { SettingsContext } from '../store/settings-context';
 const SettingsScreen = () => {
   const settingsCtx = useContext(SettingsContext);
 
+  const darkModeSettingContainer = {
+    backgroundColor: colors.extralightblack,
+    borderBottomColor: colors.gray,
+  };
+
   return (
     <ScrollView>
       <View style={styles.root}>
         <View style={styles.settingGroupContainer}>
           <View style={styles.groupIcon}>
-            <MaterialIcons name="visibility" size={30} color="black" />
-            <Text style={styles.settingsGroupText}>Visibility</Text>
+            <MaterialIcons name="visibility" size={30} color={settingsCtx.darkMode ? colors.white : colors.charcoal} />
+            <Text style={[styles.settingsGroupText, {color: settingsCtx.darkMode ? colors.white : colors.charcoal}]}>Visibility</Text>
           </View>
-          <View style={[styles.settingContainer, styles.firstSettingContainer]}>
-            <Text style={styles.settingNameText}>Dark Mode</Text>
+          <View style={[styles.settingContainer, styles.firstSettingContainer, settingsCtx.darkMode && darkModeSettingContainer]}>
+            <Text style={[styles.settingNameText, {color: settingsCtx.darkMode ? colors.white : colors.charcoal}]}>Dark Mode</Text>
             <Button 
               title={settingsCtx.darkMode ? "On" : "Off"}  
               onPress={() => settingsCtx.toggleDarkMode(!settingsCtx.darkMode)}  
             />
           </View>
-          <View style={styles.settingContainer}>
-            <Text style={styles.settingNameText}>GUI Scale</Text>
+          <View style={[styles.settingContainer, settingsCtx.darkMode && darkModeSettingContainer]}>
+            <Text style={[styles.settingNameText, {color: settingsCtx.darkMode ? colors.white : colors.charcoal}]}>GUI Scale</Text>
             <Button 
               title={`${settingsCtx.guiScale}`}  
               onPress={() => settingsCtx.updateGUIScale(2.0)}  
             />
           </View>
-          <View style={[styles.settingContainer, styles.lastSettingContainer]}>
-            <Text style={styles.settingNameText}>Units</Text>
+          <View style={[styles.settingContainer, styles.lastSettingContainer, settingsCtx.darkMode && darkModeSettingContainer]}>
+            <Text style={[styles.settingNameText, {color: settingsCtx.darkMode ? colors.white : colors.charcoal}]}>Units</Text>
             <Button 
               title={settingsCtx.metricUnits ? "Metric" : "Imperial"}  
               onPress={() => settingsCtx.toggleUnitSystem(!settingsCtx.metricUnits)}  
@@ -62,7 +67,7 @@ const styles = StyleSheet.create({
   groupIcon: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 8,
+    marginBottom: 12,
   },
 
   settingContainer: {
@@ -78,7 +83,6 @@ const styles = StyleSheet.create({
 
   settingNameText: {
     fontSize: 18,
-    color: colors.charcoal
   },
 
   firstSettingContainer: {
