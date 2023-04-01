8e4cd51031100e28526c8157e13a887d79d930d1
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2023-04-01 09:50:54 +0100

    Calendar screen dark mode

--- components/calendar/DayItem.js
+++ components/calendar/DayItem.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import { color } from 'react-native-reanimated';
 import { colors } from '../../constants/Globalstyles';
+import { useContext } from 'react';
+import { SettingsContext } from '../../store/settings-context';
 
 const DayItem = ({dayNumber, nworkouts, isToday}) => {
   const deviceWidth = Dimensions.get('window').width;
   const adjustedWidth = ((deviceWidth - 20) / 7);
-
+  const settingsCtx = useContext(SettingsContext);
   
   // Calculate the width of the day boxes so 7 fit per row on any device
   const containerStyles = {
@@ -17,7 +19,7 @@ const DayItem = ({dayNumber, nworkouts, isToday}) => {
 
   return (
     <View style={[styles.container, containerStyles]}>
-      <Text style={[styles.text, isToday && {color: colors.white}]}>{dayNumber}</Text>
+      <Text style={[styles.text, isToday && {color: colors.white}, settingsCtx.darkMode && {color: colors.white}]}>{dayNumber}</Text>
       {/*<View style={styles.exerciseDotWrapper}>
         {
         Array.apply(null, Array(nworkouts)).map(function (x, i) { return i; }).map((i) => {
