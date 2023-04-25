7187fc256d28a2fa0291eee8f8012d1aa6f51d81
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2023-04-25 19:13:53 +0100

    Enhancement: DayPadItem also renders workout dots

--- components/calendar/DayPadItem.js
+++ components/calendar/DayPadItem.js
@@ -1,9 +1,23 @@
 import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import { useState, useEffect } from 'react';
+import { workoutDB } from '../../database/localDB';
 import { colors } from '../../constants/Globalstyles';
 
-const DayPadItem = ({dayNumber, isToday}) => {
+const DayPadItem = ({dayNumber, date, isToday}) => {
   const deviceWidth = Dimensions.get('window').width;
   const adjustedWidth = ((deviceWidth - 20) / 7);
+  const [nWorkouts, setNWorkouts] = useState(null);
+
+  useEffect(() => {
+    workoutDB.transaction(tx => {
+      tx.executeSql(
+        "SELECT COUNT(*) FROM workouts WHERE year == (?) AND month == (?) AND day == (?)",
+        [date.getFullYear(), date.getMonth()+1, dayNumber],
+        (tx, result) => setNWorkouts(parseInt(Object.values(result.rows._array[0])[0])),
+        (tx, error) => console.warn(`[Error in DayPadItem.js] ${error}`)
+      );
+    });
+  }, [date]);
   
   // Calculate the width of the day boxes so 7 fit per row on any device
   const containerStyles = {
@@ -16,6 +30,13 @@ const DayPadItem = ({dayNumber, isToday}) => {
   return (
     <View style={[styles.container, containerStyles]}>
       <Text style={styles.text}>{dayNumber}</Text>
+      {!isNaN(parseInt(nWorkouts)) && (
+        <View style={styles.exerciseDotWrapper}>
+        {Array.apply(null, Array(nWorkouts)).map(function (x, i) { return i; }).map((i) => {
+          return <View key={uuid.v4()} style={[styles.dot, dotBkg]}></View>;
+        })}
+        </View>
+      )}
     </View>
   )
 }
@@ -42,7 +63,6 @@ const styles = StyleSheet.create({
     },
 
     dot: {
-      backgroundColor: colors.lightorange,
       height: 6,
       width: 6,
       borderRadius: 3,
